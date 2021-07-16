e9a0fbee1ec113d4662ae49515f09b49ef95bd79
Choirul Affan <caffanap@gmail.com>
2021-07-16 21:28:50 +0700

    [ADD] Model

--- app/Pemesanan.php
+++ app/Pemesanan.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pemesanan extends Model
 {
+    protected $fillable = ['pakets_id', 'topping_pakets_id', 'total_harga', 'nama_lengkap', 'nomor_identitas', 'no_hp', 'jenkel', 'alamat', 'status_pemesanan'];
+
     //
+    public function paket()
+    {
+        return $this->belongsTo(Paket::class);
+    }
+
+    public function topping()
+    {
+        return $this->belongsTo(ToppingPaket::class);
+    }
 }
--- app/ToppingPaket.php
+++ app/ToppingPaket.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ToppingPaket extends Model
 {
+    protected $fillable = ['name', 'desc', 'harga'];
     //
 }
